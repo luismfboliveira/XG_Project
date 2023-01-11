836343b1da557e994ecc0389660ed8546952401e
luismfboliveira <luismoliveira94@gmail.com>
2023-01-11 19:19:29 +0000

    Script to flag papers based on kw sample

---
+++ flag_articles_with_keywords.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+library(openxlsx)
+
+wd <- getwd()
+
+# path to sample file
+path_to_keywords <- ''
+
+#path to collection
+path_to_file <- ''
+
+keywords <- read.xlsx(path_to_keywords) %>% as_tibble()
+
+all_data <- read.xlsx(path_to_file) %>% as_tibble()
+
+# Cleans sample
+keywords_used <-
+  keywords %>% 
+  select(DE, ID) %>% 
+  unite("new_keywords", DE:ID, na.rm = T, sep = ";", remove = F) %>%
+  mutate(new_keywords = str_replace_all(new_keywords, c(";;" = "; ")),
+         new_keywords = str_squish(new_keywords)) %>% 
+  separate_rows(new_keywords, sep = "; ") %>% 
+  separate_rows(new_keywords, sep = ";") %>% 
+  distinct(new_keywords) %>% 
+  filter(str_detect(new_keywords, "\\b")) %>% 
+  pull(new_keywords)
+
+# flags per article
+flags <- all_data %>%
+  rowid_to_column("article_id") %>% 
+  unite("new_keywords", DE:ID, na.rm = T, sep = ";", remove = F) %>%
+  mutate(new_keywords = str_squish(str_replace_all(new_keywords, ";;", "; "))) %>%
+  select(article_id, new_keywords) %>% 
+  separate_rows(new_keywords, sep = "; ") %>% 
+  separate_rows(new_keywords, sep = ";") %>% 
+  mutate(check = str_detect(new_keywords, paste(keywords_used, collapse = "|"))) %>% 
+  group_by(article_id) %>% 
+  summarise(new_keywords = paste(new_keywords, collapse = "; "),
+            has_any = any(check),
+            how_many = sum(check),
+            pct = sum(check) / n())
+
+# adds flag columns to collection
+collection_edit <- 
+  all_data %>% 
+  rowid_to_column("article_id") %>% 
+  left_join(flags, by = "article_id") %>% 
+  relocate(new_keywords:pct, .after = "ID")
+
+write.xlsx(collection_edit,
+           paste(wd, "collection_flaged_kw.xlsx", sep = "/"),
+           overwrite = T)
+  
+
+
+  
+
+
