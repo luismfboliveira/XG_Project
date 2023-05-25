190dff462b63c828de605398c4f28fe09ff75f36
oliveib <luismoliveira94@gmail.com>
2023-05-25 23:11:49 +0200

    Fix script to detect used keywords. Added words boundaries.

--- flag_articles_with_keywords.R
+++ flag_articles_with_keywords.R
@@ -4,10 +4,10 @@ library(openxlsx)
 wd <- getwd()
 
 # path to sample file
-path_to_keywords <- ''
+path_to_keywords <- 'C:\\Users\\luism\\OneDrive\\XG\\2G_SampleData.xlsx'
 
 #path to collection
-path_to_file <- ''
+path_to_file <- 'C:\\Users\\luism\\OneDrive\\XG\\2G_RawData.xlsx'
 
 keywords <- read.xlsx(path_to_keywords) %>% as_tibble()
 
@@ -26,6 +26,19 @@ keywords_used <-
   filter(str_detect(new_keywords, "\\b")) %>% 
   pull(new_keywords)
 
+  search_words <- str_c("\\b", keywords_used, "\\b", collapse = "|")
+
+flags <- all_data %>%
+  rowid_to_column("article_id") %>% 
+  unite("new_keywords", DE:ID, na.rm = T, sep = ";", remove = F) %>%
+  mutate(new_keywords = str_squish(str_replace_all(new_keywords, ";;", "; "))) %>%
+  select(article_id, new_keywords) %>% 
+  separate_rows(new_keywords, sep = "; ") %>% 
+  separate_rows(new_keywords, sep = ";")
+
+flags %>% 
+left_join(keywords_used, by = c("new_keywords" = "new_keywords")) %>% View()
+
 # flags per article
 flags <- all_data %>%
   rowid_to_column("article_id") %>% 
@@ -34,7 +47,9 @@ flags <- all_data %>%
   select(article_id, new_keywords) %>% 
   separate_rows(new_keywords, sep = "; ") %>% 
   separate_rows(new_keywords, sep = ";") %>% 
-  mutate(check = str_detect(new_keywords, paste(keywords_used, collapse = "|"))) %>% 
+  rowwise() %>% 
+  mutate(check = any(str_detect(new_keywords, regex(search_words)))) %>% 
+  #mutate(check = str_detect(new_keywords, fixed(paste(keywords_used, collapse = "|")))) %>% 
   group_by(article_id) %>% 
   summarise(new_keywords = paste(new_keywords, collapse = "; "),
             has_any = any(check),
@@ -48,6 +63,6 @@ collection_edit <-
   left_join(flags, by = "article_id") %>% 
   relocate(new_keywords:pct, .after = "ID")
 
-write.csv(collection_edit,
-           paste(wd, "collection_flaged_kw.csv", sep = "/"),
+write.xlsx(collection_edit,
+           paste(wd, "collection_flaged_kw.xlsx", sep = "/"),
            row.names = FALSE)
